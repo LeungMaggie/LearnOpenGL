d8266910fc671e4b0f1b9c06a35538c043c2dae7
Meijian Liang <maggiieleung@gmail.com>
2026-01-20 15:37:24 +0800

    10 rotating cubes

--- 06-CoordinateSystems/main.cpp
+++ 06-CoordinateSystems/main.cpp
@@ -174,15 +174,27 @@ int main(int argc, char** argv)
   glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
 
-  glm::mat4 model(1.0f);
-  model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
   unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
-  glm::mat4 view(1.0f);
-  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
   unsigned int viewLoc = glGetUniformLocation(shader.ID, "view");
+  unsigned int projectionLoc = glGetUniformLocation(shader.ID, "projection");
+
+  glm::mat4 view(1.0f);
+  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -4.0f));
   glm::mat4 projection(1.0f);
   projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
-  unsigned int projectionLoc = glGetUniformLocation(shader.ID, "projection");
+
+  glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f), 
+    glm::vec3( 2.0f,  5.0f, -15.0f), 
+    glm::vec3(-1.5f, -2.2f, -2.5f),  
+    glm::vec3(-3.8f, -2.0f, -12.3f),  
+    glm::vec3( 2.4f, -0.4f, -3.5f),  
+    glm::vec3(-1.7f,  3.0f, -7.5f),  
+    glm::vec3( 1.3f, -2.0f, -2.5f),  
+    glm::vec3( 1.5f,  2.0f, -2.5f), 
+    glm::vec3( 1.5f,  0.2f, -1.5f), 
+    glm::vec3(-1.3f,  1.0f, -1.5f)  
+  };
 
   float mixValue = 0.2f;
   glEnable(GL_DEPTH_TEST);
@@ -202,14 +214,19 @@ int main(int argc, char** argv)
     shader.use();
     shader.set_float("mixValue", mixValue);
     
-    model = glm::mat4(1.0f);
-    model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
     glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    for (unsigned int i = 0; i < 10; ++i)
+    {
+      glm::mat4 model(1.0f);
+      model = glm::translate(model, cubePositions[i]);
+      float angle = 20.0f * (i + 1);
+      model = glm::rotate(model, glm::radians(angle) * (float)glfwGetTime(), glm::vec3(1.0f, 0.3f, 0.5f));
+      glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+    }
 
     glfwSwapBuffers(window);
     glfwPollEvents();
